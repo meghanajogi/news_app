614efc398be05018366d3d93b9fa8e3693c775cf
meghana <meghanajogi77@gmail.com>
2025-08-19 13:12:29 +0530

    Initial commit, created news list screen and details screen with static data

---
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.news_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.news_app.presentation.NewsDetailScreen
+import com.example.news_app.presentation.NewsListScreen
+import com.example.news_app.ui.theme.News_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            News_appTheme {
+                Surface(color = Color.Transparent) {
+                    AppNavigation()
+                }
+                /*Scaffold(modifier = Modifier.fillMaxSize()) {innerPadding->
+
+                  *//*  Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*//*
+                }*/
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    News_appTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "list") {
+        composable("list") {
+            NewsListScreen(navController)
+        }
+        composable(
+            route = "detail/{itemId}",
+            arguments = listOf(navArgument("itemId") {
+                type = NavType.IntType
+            })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            NewsDetailScreen(itemId = itemId)
+        }
+    }
+}

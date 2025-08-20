3b3193ec59d3e63a16b2ed3dd7fbf9f8fcd49617
meghana <meghanajogi77@gmail.com>
2025-08-20 19:35:27 +0530

    Add flowOn(Dispatchers.IO) to run the API call in background thread

--- app/src/main/java/com/example/news_app/data/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news_app/data/repository/NewsRepository.kt
@@ -3,14 +3,16 @@ package com.example.news_app.data.repository
 import com.example.news_app.data.model.NewsItem
 import com.example.news_app.network.ApiService
 import com.example.news_app.utils.Resource
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NewsRepository  @Inject constructor(private val apiService: ApiService) {
-  fun getNews(): Flow<Resource<List<NewsItem?>>> = flow {
+  fun getNews(): Flow<Resource<List<NewsItem>>> = flow {
       emit(Resource.Loading())
       try {
           val response = apiService.getPosts()
@@ -18,9 +20,9 @@ class NewsRepository  @Inject constructor(private val apiService: ApiService) {
       } catch (e: Exception) {
           emit(Resource.Error("Failed to load news: ${e.localizedMessage ?: "Unknown error"}"))
       }
-  }
+  }.flowOn(Dispatchers.IO)
 
-    fun getItemById(id: Int): Flow<Resource<NewsItem?>> = flow {
+    fun getItemById(id: Int): Flow<Resource<NewsItem>> = flow {
         emit(Resource.Loading())
         try {
             val response =  apiService.getPostById(id)
@@ -28,5 +30,5 @@ class NewsRepository  @Inject constructor(private val apiService: ApiService) {
         } catch (e: Exception) {
             emit(Resource.Error("Failed to load news: ${e.localizedMessage ?: "Unknown error"}"))
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }

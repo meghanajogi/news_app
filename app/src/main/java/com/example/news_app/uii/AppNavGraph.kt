9c501252243809ba8cbcc34f22af51455a2f13cb
meghana <meghanajogi77@gmail.com>
2025-08-22 01:27:23 +0530

    Create app nav graph

---
+++ app/src/main/java/com/example/news_app/uii/AppNavGraph.kt
@@ -0,0 +1,30 @@
+package com.example.news_app.uii
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.news_app.uii.view.NewsDetailScreen
+import com.example.news_app.uii.view.NewsListScreen
+
+
+@Composable
+fun AppNavGraph() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "list") {
+        composable("list") {
+            NewsListScreen(navController)
+        }
+        composable(
+            route = "detail/{itemId}",
+            arguments = listOf(navArgument("itemId") {
+                type = NavType.IntType
+            })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            NewsDetailScreen(navController,itemId = itemId)
+        }
+    }
+}
